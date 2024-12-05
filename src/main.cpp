e81ac19b61d1caf1cd2f43f247acd3a491de8dba
jungin7612 <jungin7612@gmail.com>
2024-12-05 20:00:42 +0900

    feat: pokemon info

--- src/main.cpp
+++ src/main.cpp
@@ -11,37 +11,108 @@ public:
         type = t;
         damage = d;
         maxTry = m;
+        currentTry = m;
     }
+    std::string getName() const { return name; }
+    std::string getType() const { return type; }
+    int getDamage() const { return damage; }
+    int getCurrentTry() const { return currentTry; }
+    int getMaxTry() const { return maxTry; }
 
 private:
     std::string name;
     std::string type;
     int damage;
     int maxTry;
+    int currentTry;
 };
 
 class Pokemon
 {
 public:
+    Pokemon() {}
     Pokemon(int no, std::string n, int m, std::string t, Skill s1, Skill s2, Skill s3, Skill s4)
     {
         number = no;
         name = n;
         max_hp = m;
+        current_hp = m;
         type = t;
         skill_list[0] = s1;
         skill_list[1] = s2;
         skill_list[2] = s3;
         skill_list[3] = s4;
     }
+    std::string getName() const { return name; }
+    std::string getType() const { return type; }
+    int getHP() const { return current_hp; }
+    const Skill *getSkills() const { return skill_list; }
 
 private:
     int number;
     std::string name;
     int max_hp;
+    int current_hp;
     std::string type;
     Skill skill_list[4];
 };
+
+void drawRowLine()
+{
+    std::cout << "+";
+    for (int i = 0; i < 61; ++i)
+    {
+        std::cout << "-";
+    }
+    std::cout << "+" << std::endl;
+}
+
+void printTitle(const std::string &title)
+{
+    drawRowLine();
+    std::cout << "| " << title;
+    for (size_t i = 0; i < 61 - title.length() - 2; ++i)
+    {
+        std::cout << " ";
+    }
+    std::cout << " |" << std::endl;
+    drawRowLine();
+}
+
+void printTwoColumnContent(const Pokemon &leftPokemon, const Pokemon &rightPokemon)
+{
+    auto printRow = [](const std::string &left, const std::string &right, size_t leftWidth, size_t rightWidth)
+    {
+        std::cout << "| " << left;
+        for (size_t i = 0; i < leftWidth - left.length(); ++i)
+        {
+            std::cout << " ";
+        }
+        std::cout << " | " << right;
+        for (size_t i = 0; i < rightWidth - right.length(); ++i)
+        {
+            std::cout << " ";
+        }
+        std::cout << " |" << std::endl;
+    };
+
+    // 포켓몬 기본 정보 출력
+    printRow(leftPokemon.getName(), rightPokemon.getName(), 28, 28);
+    printRow("Type: " + leftPokemon.getType(), "Type: " + rightPokemon.getType(), 28, 28);
+    printRow("HP: " + std::to_string(leftPokemon.getHP()), "HP: " + std::to_string(rightPokemon.getHP()), 28, 28);
+
+    // 포켓몬 스킬 출력
+    const Skill *leftSkills = leftPokemon.getSkills();
+    const Skill *rightSkills = rightPokemon.getSkills();
+    for (size_t i = 0; i < 4; ++i)
+    {
+        printRow("(" + std::to_string(i) + ") " + leftSkills[i].getName(), "(" + std::to_string(i) + ") " + rightSkills[i].getName(), 28, 28);
+        printRow("  - Type: " + leftSkills[i].getType(), "  - Type: " + rightSkills[i].getType(), 28, 28);
+        printRow("  - Damage: " + std::to_string(leftSkills[i].getDamage()), "  - Damage: " + std::to_string(rightSkills[i].getDamage()), 28, 28);
+        printRow("  - Count: " + std::to_string(leftSkills[i].getCurrentTry()) + "(" + std::to_string(leftSkills[i].getMaxTry()) + ")",
+                 "  - Count: " + std::to_string(rightSkills[i].getCurrentTry()) + "(" + std::to_string(rightSkills[i].getMaxTry()) + ")", 28, 28);
+    }
+}
 
 int main()
 {
@@ -74,19 +145,26 @@ int main()
     Pokemon Charmander(3, "Charmander", 39, "Fire", Tackle, Flamethrower, Dig, Heat_Wave);
     Pokemon Palkia(4, "Palkia", 90, "Water", Hydro_pump, Earth_Power, Surf, Spatial_Rend);
 
+    Pokemon pokemons[] = {Pikachu, Dratini, Eevee, Charmander, Palkia};
+
     int Pokemon1;
     int Pokemon2;
-    std::cout << "Choose a Pokemon(0~4): ";
+
+    std::cout << "Choose a Pokemon (0~4): ";
     std::cin >> Pokemon1;
-    std::cout << std::endl;
-    std::cout << "Choose a Pokemon(0~4): ";
+    std::cout << "Choose a Pokemon (0~4): ";
     std::cin >> Pokemon2;
 
     if (Pokemon1 == Pokemon2)
     {
         std::cout << "You have to choose Pokemons different from each other." << std::endl;
-        exit(0);
+        return 0;
     }
+
+    std::string title = "2024-02 Object-Oriented Programming Pokemon Master";
+    printTitle(title);
+    printTwoColumnContent(pokemons[Pokemon1], pokemons[Pokemon2]);
+    drawRowLine();
 
     return 0;
 }
